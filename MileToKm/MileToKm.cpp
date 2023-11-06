207774457422040edbf9044bf6d05e11e7d73325
QIUXIANG1126 <4b1g0161@stust.edu.tw>
2023-11-06 19:53:44 +0800

    first commit

---
+++ MileToKm/MileToKm.cpp
@@ -0,0 +1,9 @@
+#include <iostream>    
+#include <iomanip>    
+using namespace std;    
+    
+int main(){    
+    int a;    
+    cin >> a;    
+    cout << fixed << setprecision(1) << double(a)*1.6<<endl;  
+}
